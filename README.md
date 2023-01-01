a9d9e629c272a96866a5304e0185793b2d177927
Anton <1762282883@qq.com>
2023-01-01 15:49:40 +0800

    AntonaStandard-1.0.0

---
+++ README.md
@@ -0,0 +1,38 @@
+# AntonaStandard
+
+#### 介绍
+自定义的库，里面编写了一些常用的工具
+
+#### 软件架构
+软件架构说明
+
+
+#### 安装教程
+
+1.include分支提供所有的头文件和源文件，下载到指定路径以后添加该路径，或者将需要文件添加到项目中即可
+
+#### 使用说明
+
+1.详见 VSCode_CMake引入说明.txt
+
+#### 历史版本
+2022/12/30 AntonaStandard-v-1.0.0 添加了常用异常类库Exception.h和事件委托类库Delegate.h
+
+2023/1/1     AntonaStandard-v-1.0.1 更新了Delegate.h详细请看具体文档
+
+#### 参与贡献
+
+1.  Fork 本仓库
+2.  新建 Feat_xxx 分支
+3.  提交代码
+4.  新建 Pull Request
+
+
+#### 特技
+
+1.  使用 Readme\_XXX.md 来支持不同的语言，例如 Readme\_en.md, Readme\_zh.md
+2.  Gitee 官方博客 [blog.gitee.com](https://blog.gitee.com)
+3.  你可以 [https://gitee.com/explore](https://gitee.com/explore) 这个地址来了解 Gitee 上的优秀开源项目
+4.  [GVP](https://gitee.com/gvp) 全称是 Gitee 最有价值开源项目，是综合评定出的优秀开源项目
+5.  Gitee 官方提供的使用手册 [https://gitee.com/help](https://gitee.com/help)
+6.  Gitee 封面人物是一档用来展示 Gitee 会员风采的栏目 [https://gitee.com/gitee-stars/](https://gitee.com/gitee-stars/)
---
+++ include/Exception.h
@@ -0,0 +1,127 @@
+<<<<<<< HEAD
+#ifndef EXCEPTION_H 
+#define EXCEPTION_H
+#include <stdexcept>
+namespace AntonaStandard{
+	#define Exception_VERSION "1.1.0"
+	#define Exception_ESIT_TIME "2022/12/29"
+	#define Exception_AUTHOR "Anton"
+}
+/*
+*   Decoded by utf-8
+*   2022/12/19  1.0.0 初步实现
+*	2022/12/19  1.1.0 添加NotFound_Error 用于标记未遍历到目标值的错误
+*/
+namespace AntonaStandard{
+	class  Uninitialization_Error;
+	class  WrongArgument_Error;
+	class  UnlawfulOperation_Error;
+	class  Unkown_ErrorUnkown_Error ;
+	class  SystemCollapse_Error;
+	class  NullPointer_Error;
+	class  NotFound_Error;
+	
+}
+
+// 类的声明，和部分定义
+namespace AntonaStandard{
+	class Uninitialization_Error :public std::logic_error {
+	public:
+		Uninitialization_Error(const char* msg) :std::logic_error(msg) {};
+	};
+	class WrongArgument_Error :public std::logic_error {
+	public:
+		WrongArgument_Error(const char* msg) :std::logic_error(msg) {};
+	};
+	class UnlawfulOperation_Error :public std::logic_error {
+	public:
+		UnlawfulOperation_Error(const char* msg) :std::logic_error(msg) {};
+	};
+	class Unkown_Error :public std::logic_error {
+	public:
+		Unkown_Error(const char* msg) :std::logic_error(msg) {};
+	};
+	class SystemCollapse_Error:public std::runtime_error{
+	public:
+		SystemCollapse_Error(const char* msg):std::runtime_error(msg){};
+	};
+	class NullPointer_Error:public std::logic_error{
+	public:
+		NullPointer_Error(const char* msg):std::logic_error(msg){};
+	};
+	class NotFound_Error:public std::logic_error{
+	public:
+		NotFound_Error(const char* msg):std::logic_error(msg){};
+	};
+}
+
+
+
+#endif 
+
+
+
+=======
+#ifndef EXCEPTION_H 
+#define EXCEPTION_H
+#include <stdexcept>
+namespace AntonaStandard{
+	#define Exception_VERSION "1.1.0"
+	#define Exception_ESIT_TIME "2022/12/29"
+	#define Exception_AUTHOR "Anton"
+}
+/*
+*   Decoded by utf-8
+*   2022/12/19  1.0.0 初步实现
+*	2022/12/19  1.1.0 添加NotFound_Error 用于标记未遍历到目标值的错误
+*/
+namespace AntonaStandard{
+	class  Uninitialization_Error;
+	class  WrongArgument_Error;
+	class  UnlawfulOperation_Error;
+	class  Unkown_ErrorUnkown_Error ;
+	class  SystemCollapse_Error;
+	class  NullPointer_Error;
+	class  NotFound_Error;
+	
+}
+
+// 类的声明，和部分定义
+namespace AntonaStandard{
+	class Uninitialization_Error :public std::logic_error {
+	public:
+		Uninitialization_Error(const char* msg) :std::logic_error(msg) {};
+	};
+	class WrongArgument_Error :public std::logic_error {
+	public:
+		WrongArgument_Error(const char* msg) :std::logic_error(msg) {};
+	};
+	class UnlawfulOperation_Error :public std::logic_error {
+	public:
+		UnlawfulOperation_Error(const char* msg) :std::logic_error(msg) {};
+	};
+	class Unkown_Error :public std::logic_error {
+	public:
+		Unkown_Error(const char* msg) :std::logic_error(msg) {};
+	};
+	class SystemCollapse_Error:public std::runtime_error{
+	public:
+		SystemCollapse_Error(const char* msg):std::runtime_error(msg){};
+	};
+	class NullPointer_Error:public std::logic_error{
+	public:
+		NullPointer_Error(const char* msg):std::logic_error(msg){};
+	};
+	class NotFound_Error:public std::logic_error{
+	public:
+		NotFound_Error(const char* msg):std::logic_error(msg){};
+	};
+}
+
+
+
+#endif 
+
+
+
+>>>>>>> ecea601 (AntonaStandard-1.0.1版本修改)
